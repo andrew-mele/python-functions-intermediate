08a50268f77f72b94661aa8c5d362ae7f425fd68
Andrew <79373316+andrew-mele@users.noreply.github.com>
2022-02-13 14:18:54 -0800

    Update comments
    Updated comments to reflect questions

--- mele-andrew-functions-intermediate.py
+++ mele-andrew-functions-intermediate.py
@@ -1,5 +1,5 @@
 #QUESTION 1
-
+#Update Values in Dictionaries and Lists
 x = [ [5,2,3], [10,8,9] ] 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -12,22 +12,29 @@ sports_directory = {
 z = [ {'x': 10, 'y': 20} ]
 
 #part1
+#Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x[1][0] = 15
 print(x)
 
 #part2
+#Change the last_name of the first student from 'Jordan' to 'Bryant'
 students[0]['last_name'] = 'Bryant'
 print(students[0])
 
 #part3
+#In the sports_directory, change 'Messi' to 'Andres'
 sports_directory['soccer'][0] = 'Andres'
 print(sports_directory)
 
 #part4
+#Change the value 20 in z to 30
 z[0]['y'] = 30
 print(z)
 
 #QUESTION 2
+"""Create a function iterateDictionary(some_list) that, given a list of dictionaries, 
+the function loops through each dictionary in the list and prints each key and the associated value. 
+For example, given the following list: """
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
     {'first_name' : 'John', 'last_name' : 'Rosales'},
@@ -41,6 +48,9 @@ def iterateDictionary(students):
 iterateDictionary(students)
 
 #QUESTION 3
+"""Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, 
+the function prints the value stored in that key for each dictionary. 
+For example, iterateFirstName('first_name', students) should output:"""
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
     {'first_name' : 'John', 'last_name' : 'Rosales'},
@@ -52,6 +62,8 @@ def iterateFirstName(students):
         print(name['first_name'])
 
 iterateFirstName(students)
+
+#And iterateLastName('last_name', students) should output:
 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -66,6 +78,8 @@ def iterateLastName(students):
 iterateLastName(students)
 
 #QUESTION 4
+"""Create a function printInfo(some_dict) that given a dictionary whose values are all lists, 
+prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:"""
 dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
